d938025e34825118a08bed75f67788335c686ec9
snios <nicklas.oest@gmail.com>
2022-12-05 09:01:15 +0100

    removed day 2 stuff

--- 1/1.py
+++ 1/1.py
@@ -1,14 +1,12 @@
 
 def solve(data):
     array = data.split("\n")
-
-    results = []
+    
     count = 0
     for x in array:
         if x.isdigit():
             count += int(x)
         else:
-            results.append(count)
             count = 0
 
     print(count)
